faf90e8cac093799a0268198dc2e9dffc5087edf
cat1989 <562702740@qq.com>
2024-08-30 11:27:54 +0800

    feat: view version

--- src/index.ts
+++ src/index.ts
@@ -45,27 +45,65 @@ const copyDirSync = (src: string, dest: string) => {
     })(src, dest)
 }
 
-const [,,name = ''] = process.argv
-if (name === '') {
-    process.exit()
+const getArgs = (argv: string[]) => {
+    const args: Record<string, string | boolean> = {}
+    argv.forEach((arg) => {
+        if (arg.startsWith('--')) {
+            const [key, value] = arg.substring('--'.length).split('=')
+            args[key] = value ?? true
+        }
+        else if (arg.startsWith('-')) {
+            arg.substring('-'.length).split('').forEach((key) => {
+                args[key] = true
+            })
+        }
+        else {
+            args[arg] = arg
+        }
+    })
+    return args
+}
+
+const create = () => {
+    const [,,name = '',...rest] = process.argv
+    const args = getArgs(rest)
+    if (name !== '') {
+        const encoding = 'utf-8'
+        const cwd = process.cwd()
+        const root = path.resolve(cwd, name)
+        const template = args.template ?? 'vue2.6'
+        const templatePath = path.resolve(__dirname, `../lib/templates/${template}`)
+        if (fs.existsSync(templatePath)) {
+            if (!fs.existsSync(root)) {
+                fs.mkdirSync(root)
+            }
+            const packageJson = {
+                ...createPackageJson(name),
+                ...JSON.parse(fs.readFileSync(path.resolve(templatePath, 'package.json'), {
+                    encoding,
+                })),
+            }
+            fs.writeFileSync(path.resolve(root, 'package.json'), JSON.stringify(packageJson, null, '  '), {
+                encoding,
+            })
+            copyDirSync(path.resolve(templatePath, 'files'), root)
+            console.log('success')
+        }
+    }
+}
+
+const version = () => {
+    const encoding = 'utf-8'
+    console.log(JSON.parse(fs.readFileSync(path.resolve(__dirname, '../package.json'), {
+        encoding,
+    })).version)
+}
+
+const [,,...argv] = process.argv
+const args = getArgs(argv)
+if (args.v || args.version) {
+    version()
 }
 else {
-    const encoding = 'utf-8'
-    const cwd = process.cwd()
-    const root = path.resolve(cwd, name)
-    if (!fs.existsSync(root)) {
-        fs.mkdirSync(root)
-    }
-    const template = path.resolve(__dirname, '../lib/templates/vue2.6')
-    const packageJson = {
-        ...createPackageJson(name),
-        ...JSON.parse(fs.readFileSync(path.resolve(template, 'package.json'), {
-            encoding,
-        })),
-    }
-    fs.writeFileSync(path.resolve(root, 'package.json'), JSON.stringify(packageJson, null, '  '), {
-        encoding,
-    })
-    copyDirSync(path.resolve(template, 'files'), root)
-    console.log('success')
+    create()
 }
